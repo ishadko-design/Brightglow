de0b5da3f177ef57e3c3949a41de64fec3328f19
ishadko <igor.shadko@gmail.com>
2026-06-06 12:08:14 -0700

    Initial Commit

---
+++ FIXR/FIXRApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIXRApp.swift
+//  FIXR
+//
+//  Created by test on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct FIXRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
